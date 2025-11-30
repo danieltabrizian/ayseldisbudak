e8a870f4210fa8c549aca8586af62345a47c3ed8
danieltabrizian <danieltabrizian@live.nl>
2025-11-30 14:48:29 +0800

    Better sizes

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,6 @@ const nextConfig = withExportImages({
   trailingSlash: true,
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 
   pageExtensions: ["js", "jsx", "ts", "tsx"],
